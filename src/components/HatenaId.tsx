9ed88e2592469a8fb076be4d24d815fcb7c4cd66
Fumiya Tokumasu <toof@toof.jp>
2025-08-29 00:23:35 +0900

    refactor: hatena id

--- src/components/HatenaId.tsx
+++ src/components/HatenaId.tsx
@@ -1,3 +1,9 @@
 export default function HatenaId({ id }: { id: string }) {
-    return <a href={`http://blog.hatena.ne.jp/${id}/`}><img src={`https://cdn.profile-image.st-hatena.com/users/${id}/profile.png`} alt={`${id} icon`} width={16} height={16}/>id:{id}</a>
+    const href = `https://blog.hatena.ne.jp/${id}/`;
+    const imgSrc = `https://cdn.profile-image.st-hatena.com/users/${id}/profile.png`;
+    return (
+        <a href={href}>
+            <img src={imgSrc} alt={`hatena ${id} icon`} width={16} height={16} />id:{id}
+        </a>
+    );
 }
